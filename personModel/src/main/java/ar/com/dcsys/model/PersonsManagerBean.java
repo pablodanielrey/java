4d3f07d716baf977aaed22c2a3114a0705dd0420
pablodanielrey <pablo@econo.unlp.edu.ar>
2013-11-24 02:58:36 -0300

    ajuste para que el modelo injecte el personDAO pero sin nombrarlo. ya que ahora va por inclusion de proyectos.

--- personModel/src/main/java/ar/com/dcsys/model/PersonsManagerBean.java
+++ personModel/src/main/java/ar/com/dcsys/model/PersonsManagerBean.java
@@ -10,7 +10,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.inject.Singleton;
 
 import ar.com.dcsys.data.person.Person;
@@ -36,7 +35,7 @@ public class PersonsManagerBean implements PersonsManager {
 	private final PersonDAO personDAO;
 	
 	@Inject
-	public PersonsManagerBean(@Named("personHsqlDAO") PersonDAO personDAO) {
+	public PersonsManagerBean(PersonDAO personDAO) {
 		this.personDAO = personDAO;
 		createCaches();
 	}
