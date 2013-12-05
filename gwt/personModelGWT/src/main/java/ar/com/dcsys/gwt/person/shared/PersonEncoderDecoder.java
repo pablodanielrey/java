dd25dbb4ddcb7f420641c954c3106e6b42c448a9
pablodanielrey <pablo@econo.unlp.edu.ar>
2013-12-04 21:53:54 -0300

    ahora uso la implemetnacion de las utilidades de autobean en vez de codificarlas internas al encoder.

--- gwt/personModelGWT/src/main/java/ar/com/dcsys/gwt/person/shared/PersonEncoderDecoder.java
+++ gwt/personModelGWT/src/main/java/ar/com/dcsys/gwt/person/shared/PersonEncoderDecoder.java
@@ -1,6 +1,5 @@
 package ar.com.dcsys.gwt.person.shared;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,6 +7,7 @@ import javax.inject.Inject;
 import ar.com.dcsys.data.person.Mail;
 import ar.com.dcsys.data.person.Person;
 import ar.com.dcsys.data.person.PersonType;
+import ar.com.dcsys.gwt.autoBeans.shared.AutoBeanUtils;
 import ar.com.dcsys.gwt.person.shared.lists.MailList;
 import ar.com.dcsys.gwt.person.shared.lists.PersonList;
 import ar.com.dcsys.gwt.person.shared.lists.PersonTypeList;
@@ -15,7 +15,7 @@ import ar.com.dcsys.gwt.person.shared.lists.PersonValueList;
 
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanCodex;
-import com.google.web.bindery.autobean.shared.AutoBeanUtils;
+
 
 public class PersonEncoderDecoder {
 
@@ -27,89 +27,11 @@ public class PersonEncoderDecoder {
 		
 	}
 	
-	/**
-	 * Generico para hacer wrapping a una lista para que funcione la serializacion de autobeans.
-	 * @param clazz
-	 * @param l
-	 * @return
-	 */
-	private <T> List<T> wrapList(Class<T> clazz, List<T> l) {
-		List<T> r = new ArrayList<T>(l.size());
-		for (T t : l) {
-			if (AutoBeanUtils.getAutoBean(t) != null) {
-				r.add(t);
-			} else {
-				r.add(personFactory.create(clazz).as());
-			}
-		}
-		return r;
-	}
-
-	/**
-	 * Codifcación genérica de una clase a String usando AutoBeanCodex
-	 * @param clazz
-	 * @param t
-	 * @return
-	 */
-	public <T> String encode(Class<T> clazz, T t) {
-		AutoBean<T> bean = AutoBeanUtils.getAutoBean(t);
-		if (bean == null) {
-			// no tiene bean asi que hay que crearlo.
-			bean = personFactory.create(clazz,t);
-		}
-		String json = AutoBeanCodex.encode(bean).getPayload();
-		return json;
-	}
-	
-	/**
-	 * Decodificiacion genérica de una clase desde un String usando AutoBeanCodex.
-	 * @param clazz
-	 * @param json
-	 * @return
-	 */
-	public <T> T decode(Class<T> clazz, String json) {
-		AutoBean<T> bean = AutoBeanCodex.decode(personFactory, clazz, json);
-		T t = bean.as();
-		return t;
-	}		
-	
-	
-	/*
-	public String encodePerson(Person person) {
-		AutoBean<Person> bean = AutoBeanUtils.getAutoBean(person);
-		String json = AutoBeanCodex.encode(bean).getPayload();
-		return json;
-	}
-	
-	public Person decodePerson(String json) {
-		AutoBean<Person> bean = AutoBeanCodex.decode(personFactory, Person.class, json);
-		Person person = bean.as();
-		return person;
-	}		
-	
-
-	public String encodeMail(Mail m) {
-		AutoBean<Mail> bean = AutoBeanUtils.getAutoBean(m);
-		String json = AutoBeanCodex.encode(bean).getPayload();
-		return json;
-	}
-
-	
-	public Mail decodeMail(String json) {
-		AutoBean<Mail> bean = AutoBeanCodex.decode(personFactory, Mail.class, json);
-		Mail m = bean.as();
-		return m;
-	}	
-	
-	*/
-	
-	
-	
 	public String encodePersonValueList(List<PersonValueProxy> pt) {
 		AutoBean<PersonValueList> aptl = personFactory.personValueList();
 		PersonValueList ptl = aptl.as();
 		
-		List<PersonValueProxy> wpt = wrapList(PersonValueProxy.class, pt);
+		List<PersonValueProxy> wpt = AutoBeanUtils.wrapList(personFactory, PersonValueProxy.class, pt);
 		ptl.setList(wpt);
 		
 		String json = AutoBeanCodex.encode(aptl).getPayload();
@@ -130,6 +52,8 @@ public class PersonEncoderDecoder {
 		PersonTypeList ptl = aptl.as();
 		ptl.setList(pt);
 
+		// no es necesario hacerle wrapping porque los elementos son enumerativos.
+		
 		String json = AutoBeanCodex.encode(aptl).getPayload();
 		return json;
 	}
@@ -147,7 +71,7 @@ public class PersonEncoderDecoder {
 		AutoBean<MailList> aptl = personFactory.mailList();
 		MailList ptl = aptl.as();
 		
-		List<Mail> wpt = wrapList(Mail.class, pt);
+		List<Mail> wpt = AutoBeanUtils.wrapList(personFactory, Mail.class, pt);
 		ptl.setList(wpt);
 		
 		String json = AutoBeanCodex.encode(aptl).getPayload();
@@ -166,7 +90,7 @@ public class PersonEncoderDecoder {
 		AutoBean<PersonList> aptl = personFactory.personList();
 		PersonList ptl = aptl.as();
 		
-		List<Person> wpt = wrapList(Person.class, pt);
+		List<Person> wpt = AutoBeanUtils.wrapList(personFactory, Person.class, pt);
 		ptl.setList(wpt);
 		
 		String json = AutoBeanCodex.encode(aptl).getPayload();
@@ -184,34 +108,7 @@ public class PersonEncoderDecoder {
 	
 	
 	
-	/**
-	 * Metodo genérico para codificar una lista de elementos de una clase en especial T.
-	 * @param clazz
-	 * @param list
-	 * @return
-	 */
-/*	public <T> String encodeList(Class<T> clazz, List<T> list) {
 
-		// para manejar el error de autobean cuando los elementos de la lista no tienen wrapping.
-		List<T> wrappedList = wrapList(clazz, list);
-		
-		AutoBean<ListContainer<T>> lc = personFactory.listContainer();
-		ListContainer<T> lcontainer = lc.as();
-		lcontainer.setList(list);
-		
-		String json = AutoBeanCodex.encode(lc).getPayload();
-		return json;
-	}
-	
-	
-	public <T> List<T> decodeList(Class<T> clazz, String list) {
-		AutoBean<ListContainer> bean = AutoBeanCodex.decode(personFactory, ListContainer.class, list);
-		ListContainer lc = bean.as();
-		List<T> rlist = lc.getElements(clazz);
-		return rlist;
-	}
-*/	
-		
 	
 	
 	
