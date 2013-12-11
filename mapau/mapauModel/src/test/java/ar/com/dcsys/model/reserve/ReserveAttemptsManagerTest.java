cbaefe328f827bea3840971bface0b8741d0cc3a
Emanuel <emanuel@emanuel-pc.(none)>
2013-12-11 20:01:47 -0300

    Termine el test de ReserveAttemptsManager

--- mapau/mapauModel/src/test/java/ar/com/dcsys/model/reserve/ReserveAttemptsManagerTest.java
+++ mapau/mapauModel/src/test/java/ar/com/dcsys/model/reserve/ReserveAttemptsManagerTest.java
@@ -21,6 +21,7 @@ import ar.com.dcsys.data.classroom.CharacteristicQuantity;
 import ar.com.dcsys.data.classroom.ClassRoom;
 import ar.com.dcsys.data.classroom.ClassRoomBean;
 import ar.com.dcsys.data.filter.TransferFilter;
+import ar.com.dcsys.data.filter.TransferFilterType;
 import ar.com.dcsys.data.group.Group;
 import ar.com.dcsys.data.group.types.GroupType;
 import ar.com.dcsys.data.person.Mail;
@@ -144,12 +145,12 @@ public class ReserveAttemptsManagerTest {
 		return classRoom;
 	}
 
-	private Subject createSubject() throws MapauException {
+	private Subject createSubject(String name) throws MapauException {
 		
 		SubjectsManager subjectsManager = getSubjectsManager();
 		
 		Subject subject = new SubjectBean();
-		subject.setName("Conta 1");
+		subject.setName(name);
 		
 		String id = subjectsManager.persist(subject);
 		
@@ -159,14 +160,14 @@ public class ReserveAttemptsManagerTest {
 		return subject;
 	}
 	
-	private Course createCourse() throws MapauException {
+	private Course createCourse(String nameSubject, String nameCourse) throws MapauException {
 		CoursesManager coursesManager = getCoursesManager();
 		
 		Course course = new CourseBean();
 		
-		Subject subject = createSubject();		
+		Subject subject = createSubject(nameSubject);		
 		course.setSubject(subject);
-		course.setName("Catedra B");
+		course.setName(nameCourse);
 		String id = coursesManager.persist(course);
 		
 		assertNotNull(course);
@@ -247,33 +248,99 @@ public class ReserveAttemptsManagerTest {
 	public void createNewAppointment() throws MapauException, PersonException {
 		ReserveAttemptsManager reserveAttemptsManager = getReserveAttemptsManager();
 
-		Date startS = new Date(today.getTime() - (dayL*2));
-		Date endS = new Date(today.getTime() + (dayL*2));
 		List<TransferFilter> filters = new ArrayList<TransferFilter>();
+		Course course = createCourse("Conta I", "Catedra A");
+		TransferFilter tfCourse = createTransferFilterCourse(course);
+		filters.add(tfCourse);
 		
-		List<AppointmentV2> appointments = reserveAttemptsManager.findAppointmentsV2By(startS, endS, filters);
+		Calendar c = Calendar.getInstance();
+		
+		Date start = new Date();	
+		c.setTime(start);
+		c.set(Calendar.SECOND,0);
+		c.set(Calendar.MILLISECOND,0);
+		start = c.getTime();
+
+		Date end = new Date(start.getTime() + (1000l * 60l * 90l));
+		c.setTime(end);
+		c.set(Calendar.SECOND,0);
+		c.set(Calendar.MILLISECOND,0);
+		end = c.getTime();			
+		
+		List<AppointmentV2> appointments = reserveAttemptsManager.findAppointmentsV2By(start, end, filters);
 		
 		assertNotNull(appointments);
 		int count = appointments.size();
+		assertEquals(count, 0);
 		
 		List<AppointmentV2> appointmentsNew = new ArrayList<>();
 
 		//creo el appointment			
-		Date start = new Date();
-		Date end = new Date(start.getTime() + (1000l * 60l * 90l));	
-		Course course = createCourse();
 		ReserveAttemptDateType reserveAttemptType = createReserveAttemptType("Final");	
+		Person owner = createPerson();	
+
+		ClassRoom classRoom = createClassRoom("102", new ArrayList<CharacteristicQuantity>());
 		
-		AppointmentV2 app = createAppointment(start, end, course, reserveAttemptType);
+		List<ClassRoom> classRooms = new ArrayList<ClassRoom>();
+		classRooms.add(classRoom);
+		List<Course> courses = new ArrayList<Course>();
+		courses.add(course);		
+		Area area = createArea("Detise",courses,classRooms);		
+		String description = "Descripción";
+		String studentGroup = "Comision 1";
+		
+		AppointmentV2 app = new AppointmentV2Bean();
+		
+		app.setArea(area);
+		app.setCharacteristics(new ArrayList<CharacteristicQuantity>());
+		app.setCourse(course);
+		app.setDescription(description);
+		app.setStart(start);
+		app.setEnd(end);
+		app.setOwner(owner);
+		app.setR(null);
+		app.setRad(null);
+		app.setRaType(reserveAttemptType);
+		app.setStudentGroup(studentGroup);		
+		app.setRelatedAppointments(new ArrayList<String>());
+		app.setRelatedPersons(new ArrayList<Person>());
+		
 		appointmentsNew.add(app);
 		
 		reserveAttemptsManager.createNewAppointments(appointmentsNew);
-		count += 1;
 		
 		// verifico que lo agrego a la base
-		appointments = reserveAttemptsManager.findAppointmentsV2By(startS, endS, filters);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(start, end, filters);
 		assertNotNull(appointments);
-		assertEquals(appointments.size(), count);
+		assertEquals(appointments.size(), count + 1);
+		
+		app = appointments.get(0);		
+		assertNotNull(app);
+		
+		assertNotNull(app.getArea());
+		assertEquals(app.getArea().getId(), area.getId());
+		
+		assertNotNull(app.getCharacteristics());
+		assertEquals(app.getCharacteristics().size(),0);
+		
+		assertNotNull(app.getCourse());
+		assertEquals(course.getId(), app.getCourse().getId());
+		
+		assertEquals(app.getDescription(), description);
+		
+		assertNotNull(app.getStart());
+		assertEquals(app.getStart().getTime(),start.getTime());
+		
+		assertNotNull(app.getEnd());
+		assertEquals(app.getEnd().getTime(),end.getTime());
+				
+		assertNotNull(app.getRad());
+		
+		assertNotNull(app.getRaType());
+		assertEquals(app.getRaType().getId(), reserveAttemptType.getId());
+		
+		assertEquals(app.getStudentGroup(), studentGroup);
+		
 	}
 	
 	
@@ -286,7 +353,7 @@ public class ReserveAttemptsManagerTest {
 		
 
 		// inicalizo los datos que voy a utilizar
-		Course course = createCourse();
+		Course course = createCourse("Conta I", "Catedra A");
 		ReserveAttemptDateType reserveAttemptTypeFinal = createReserveAttemptType("Final");	
 		ReserveAttemptDateType reserveAttemptTypeParcial = createReserveAttemptType("Parcial");	
 		
@@ -432,8 +499,326 @@ public class ReserveAttemptsManagerTest {
 		appointments = reserveAttemptsManager.findAllAppointmentsBy(appOriginal, dates, true);
 		assertNotNull(appointments);
 		assertEquals(appointments.size(), countCheckHour);		
+		
+	}
+	
+	@Test
+	public void findAllFilters() throws MapauException {
+		ReserveAttemptsManager reserveAttemptsManager = getReserveAttemptsManager();
+		
+		List<TransferFilterType> filters = reserveAttemptsManager.findAllFilters();
+		
+		assertNotNull(filters);
+			
+	}
+	
+	
+	private class TransferFilterImp implements TransferFilter {
+		private TransferFilterType type;
+		private String param;
+		
+		@Override
+		public TransferFilterType getType() {
+			return type;
+		}
 
+		@Override
+		public void setType(TransferFilterType type) {
+			this.type = type;
+		}
+
+		@Override
+		public String getParam() {
+			return param;
+		}
+
+		@Override
+		public void setParam(String param) {
+			this.param = param;
+		}
+		
+	}
+	
+	private TransferFilter createTransferFilterDate(Date start, Date end) {
+
+		TransferFilter tfp = new TransferFilterImp();
+        
+        tfp.setType(TransferFilterType.DATE);
+        
+        String startStr = String.valueOf(start.getTime());
+        String endStr = String.valueOf(end.getTime());
+        tfp.setParam(startStr + ";" + endStr);
+        
+        return tfp;		
+	}
+	
+	private TransferFilter createTransferFilterCourse(Course c) {
+
+		TransferFilter tfp = new TransferFilterImp();
+        
+        tfp.setType(TransferFilterType.COURSE);        
+        tfp.setParam(c.getId());
+        
+        return tfp;		
+	}
+	
+	private TransferFilter createTransferFilterClassRoom(ClassRoom cr) {
+
+		TransferFilter tfp = new TransferFilterImp();
+        
+        tfp.setType(TransferFilterType.CLASSROOM);        
+        tfp.setParam(cr.getId());
+        
+        return tfp;		
+	}
+	
+	private Date setTime(Date date, int hour, int min, int sec, int mili) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		c.set(Calendar.HOUR_OF_DAY, hour);
+		c.set(Calendar.MINUTE, min);
+		c.set(Calendar.SECOND,sec);
+		c.set(Calendar.MILLISECOND,mili);
+		return c.getTime();		
+	}
+	
+	@Test
+	public void findAppointmentsV2ByFilters() throws MapauException, PersonException {
+		ReserveAttemptsManager reserveAttemptsManager = getReserveAttemptsManager();
+		
+
+		ReserveAttemptDateType reserveAttemptTypeFinal = createReserveAttemptType("Final");	
+		
+		//creo el filtro de fecha 
+		Date start = new Date();
+		Date end = new Date(start.getTime() + (dayL * 7));
+		
+		start = setTime(start, 0, 0, 0, 0);
+		end = setTime(end, 23, 59, 59, 999);	
+		
+		TransferFilter tfDate = createTransferFilterDate(start, end);
+		
+		
+		//creo el filtro de course
+		Course course = createCourse("Conta I", "Catedra A");
+		assertNotNull(course);
+		assertNotNull(course.getId());
+		TransferFilter tfCourse = createTransferFilterCourse(course);
+		
+		// cargo los filtros y realizo la busqueda
+		List<TransferFilter> filters = new ArrayList<TransferFilter>();
+		filters.add(tfDate);
+		filters.add(tfCourse);
+		
+		List<AppointmentV2> appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		
+		int count = appointments.size();
+		assertEquals(count, 0);
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		int countAll = appointments.size();
+
+		/* **********************************************************************************************************************
+		 * creo un appointment que tenga el course creado al inicio y que este entre las fechas start y end
+		 * ******************************************************************************************************************** */
+		Date newStart = new Date();
+		newStart = setTime(newStart, 8, 0, 0, 0);
+		Date newEnd = new Date();
+		newEnd = setTime(newEnd, 11, 30, 0, 0);
+		
+		List<AppointmentV2> appointmentsNew = new ArrayList<AppointmentV2>();
+		AppointmentV2 app = createAppointment(newStart, newEnd, course, reserveAttemptTypeFinal);
+		appointmentsNew.add(app);
+		reserveAttemptsManager.createNewAppointments(appointmentsNew);
+		
+		//realizo la busqueda con el filtro de fecha
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);		
+		assertEquals(appointments.size(), countAll + 1);		
+		
+		//realizo la busqueda con todos los filtros
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfCourse);
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		assertEquals(appointments.size(), count + 1);	
+		
+		
+		/* **********************************************************************************************************************
+		 * creo un appointment que tenga un nuevo course y que este entre las fechas start y end
+		 * ******************************************************************************************************************** */
+		newStart = new Date();
+		newStart = setTime(newStart, 12, 0, 0, 0);
+		newEnd = new Date();
+		newEnd = setTime(newEnd, 15, 30, 0, 0);
+		
+		Course courseNew = createCourse("Administracion I", "Catedra B");
+		
+		app = createAppointment(newStart, newEnd, courseNew, reserveAttemptTypeFinal);
+		appointmentsNew.clear();
+		appointmentsNew.add(app);
+		reserveAttemptsManager.createNewAppointments(appointmentsNew);		
+		
+		//realizo la busqueda con el filtro de fecha
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		assertEquals(appointments.size(), countAll + 2);		
+		
+		//realizo la busqueda con todos los filtros
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfCourse);
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		assertEquals(appointments.size(), count + 1);	
+		
+
+		/* **********************************************************************************************************************
+		 * creo un appointment que tenga el course creado al inicio y que no este entre las fechas start y end
+		 * ******************************************************************************************************************** */
+		newStart = new Date(end.getTime() + dayL);
+		newStart = setTime(newStart, 8, 0, 0, 0);
+		newEnd = new Date(newStart.getTime());
+		newEnd = setTime(newEnd, 12, 0, 0, 0);
+		
+		app = createAppointment(newStart, newEnd, course, reserveAttemptTypeFinal);
+		appointmentsNew.clear();
+		appointmentsNew.add(app);
+		reserveAttemptsManager.createNewAppointments(appointmentsNew);	
+		
+		//realizo la busqueda con el filtro de fecha
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		assertEquals(appointments.size(), countAll + 2);		
+		
+		//realizo la busqueda con todos los filtros
+		filters = new ArrayList<TransferFilter>();
+		filters.add(tfCourse);
+		filters.add(tfDate);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(filters);
+		assertEquals(appointments.size(), count + 1);	
+		
 		
 		
 	}
+	
+	
+	@Test
+	public void modifyAndDeleteAppointment() throws MapauException, PersonException {
+		ReserveAttemptsManager reserveAttemptsManager = getReserveAttemptsManager();
+
+		List<TransferFilter> filters = new ArrayList<TransferFilter>();
+		Course course = createCourse("Conta I", "Catedra A");
+		TransferFilter tfCourse = createTransferFilterCourse(course);
+		filters.add(tfCourse);
+		
+		Calendar c = Calendar.getInstance();
+		
+		Date start = new Date();	
+		c.setTime(start);
+		c.set(Calendar.MILLISECOND, 333);
+		start = c.getTime();
+		
+		Date end = new Date(start.getTime() + (1000l * 60l * 95l));
+		c.setTime(end);
+		c.set(Calendar.MILLISECOND, 987);
+		end = c.getTime();
+		
+		
+		List<AppointmentV2> appointments = reserveAttemptsManager.findAppointmentsV2By(start, end, filters);
+		assertNotNull(appointments);
+		assertEquals(appointments.size(),0);
+		
+		//creo el appointment
+		ReserveAttemptDateType reserveAttemptType = createReserveAttemptType("Final");
+		AppointmentV2 app = createAppointment(start, end, course, reserveAttemptType);
+		appointments = new ArrayList<AppointmentV2>();
+		appointments.add(app);
+		reserveAttemptsManager.createNewAppointments(appointments);
+		
+		//vertifico que lo haya insertado
+		appointments = reserveAttemptsManager.findAppointmentsV2By(start, end, filters);
+		assertNotNull(appointments);
+		assertEquals(appointments.size(),1);
+		
+		//obtengo el appointment insertado y lo modifico
+		app = appointments.get(0);
+		assertNotNull(app.getRad());
+		
+		Date newStart = new Date(start.getTime() + hourL);
+		Date newEnd = new Date(end.getTime() + hourL); 
+		List<ClassRoom> classRooms = new ArrayList<ClassRoom>();
+		ClassRoom cr = createClassRoom("306", new ArrayList<CharacteristicQuantity>());
+		List<Course> courses = new ArrayList<Course>();
+		Course newCourse = createCourse("Conta II", "Catedra C");
+		Area area = createArea("SALAPROF", courses, classRooms);		
+		String description = "AppointmentModify";
+		String studentGroup = "Comision 8";
+		ReserveAttemptDateType newReserveAttemptType = createReserveAttemptType("Parcial");
+		
+		app.setArea(area);
+		app.setCharacteristics(new ArrayList<CharacteristicQuantity>());
+		app.setCourse(newCourse);
+		app.setDescription(description);
+		app.setStart(newStart);
+		app.setEnd(newEnd);
+		app.setRaType(newReserveAttemptType);
+		app.setStudentGroup(studentGroup);
+		
+		assertNotNull(app.getRad());
+		reserveAttemptsManager.modify(app);
+		
+		//busco el appointment con la fecha anterior
+		appointments = reserveAttemptsManager.findAppointmentsV2By(start, end, filters);
+		assertNotNull(appointments);
+		//me tiene que dar 0 ya que no lo deberia encontrar porque fue modificada la fecha
+		assertEquals(appointments.size(),0);
+		//busco el appointment con la nueva fecha
+		//agrego el nuevo course como fultro
+		tfCourse = createTransferFilterCourse(newCourse);
+		filters = new ArrayList<>();
+		filters.add(tfCourse);		
+		appointments = reserveAttemptsManager.findAppointmentsV2By(newStart, newEnd, filters);
+		assertNotNull(appointments);
+		assertEquals(appointments.size(),1);
+		
+		app = appointments.get(0);
+		
+		
+		//verifico que se haya modificado
+		assertNotNull(app.getArea());
+		assertEquals(app.getArea().getId(), area.getId());
+		
+		assertNotNull(app.getCharacteristics());
+		assertEquals(app.getCharacteristics().size(),0);
+		
+		assertNotNull(app.getCourse());
+		assertEquals(newCourse.getId(), app.getCourse().getId());
+		
+		assertEquals(app.getDescription(), description);
+		
+		assertNotNull(app.getStart());
+		assertEquals(app.getStart().getTime(),newStart.getTime());
+		
+		assertNotNull(app.getEnd());
+		assertEquals(app.getEnd().getTime(),newEnd.getTime());
+				
+		assertNotNull(app.getRad());
+		
+		assertNotNull(app.getRaType());
+		assertEquals(app.getRaType().getId(), newReserveAttemptType.getId());
+		
+		assertEquals(app.getStudentGroup(), studentGroup);		
+		
+		
+		//ahora lo elimino
+		reserveAttemptsManager.deleteAppointment(app);
+		appointments = reserveAttemptsManager.findAppointmentsV2By(newStart, newEnd, filters);
+		assertNotNull(appointments);
+		assertEquals(appointments.size(),0);
+	}
+	
 }
