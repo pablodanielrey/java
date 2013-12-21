499c74c1efad553a552f5648c91cf47008281e56
pablodanielrey <pablo@econo.unlp.edu.ar>
2013-12-21 16:59:00 -0300

    agregue mas datos a la session. mezcle capas pero queda mas claro de un solo vistazo a la base las sesiones que estan activas.

--- auth/shiro/src/main/java/ar/com/dcsys/auth/shiro/PostgreSqlDCSysSessionDAO.java
+++ auth/shiro/src/main/java/ar/com/dcsys/auth/shiro/PostgreSqlDCSysSessionDAO.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.security.Principal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -25,6 +26,8 @@ import org.apache.shiro.session.Session;
 import org.apache.shiro.session.UnknownSessionException;
 import org.apache.shiro.session.mgt.SimpleSession;
 import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
 
 public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 
@@ -56,7 +59,17 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 						"id varchar not null primary key," +
 						"created timestamp not null," +
 						"lastAccess timestamp not null," +
+						"stoped timestamp," +
 						"timeout bigint, " +
+						"expired boolean," +
+						
+						// estos campos se sacan de la session si es que fue guardado el subject. en caso contrario estan en null. ///
+						
+						"authenticated boolean," +
+						"principal varchar," +
+						
+						//////
+					
 						"host varchar," +
 						"session bytea not null" +
 						")");
@@ -81,8 +94,7 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 		return DriverManager.getConnection(db,user,pass);
 	}
 	
-	private byte[] serializeSession(Session s) throws IOException {
-		SimpleSession ss = (SimpleSession)s;
+	private byte[] serializeSession(SimpleSession ss) throws IOException {
 		Map data = ss.getAttributes();
 		
 		ByteArrayOutputStream bo = new ByteArrayOutputStream();
@@ -100,17 +112,55 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 	}
 	
 	
-	private void setSession(PreparedStatement st, Session session) throws SQLException, IOException {
+	private void setSession(PreparedStatement st, Session ses) throws SQLException, IOException {
+		
+		SimpleSession session = (SimpleSession)ses;
 		
 		st.setString(1,session.getId().toString());	
 		
-		st.setTimestamp(2, new Timestamp(session.getStartTimestamp().getTime()));
-		st.setTimestamp(3, new Timestamp(session.getLastAccessTime().getTime()));
-		st.setLong(4, session.getTimeout());
-		st.setString(5,session.getHost());
+		Date date = session.getStartTimestamp();
+		st.setTimestamp(2, new Timestamp(date.getTime()));
+		
+		date = session.getLastAccessTime();
+		st.setTimestamp(3, new Timestamp(date.getTime()));
+
+		date = session.getStopTimestamp();
+		if (date == null) {
+			st.setTimestamp(4,null);
+		} else {
+			st.setTimestamp(4, new Timestamp(date.getTime()));
+		}
+		
+		st.setLong(5, session.getTimeout());
+		st.setBoolean(6, session.isExpired());
+		st.setString(7,session.getHost());
+		
+		
+		/////////// en el caso de que este habilitado el subject dentro de la session entonces obtengo esos datos (solo informativo para ver las tablas mas facil) ///////////////////
+		
+		/// definido en DefaultSubjectDAO
+		Object oa = session.getAttribute(DefaultSubjectContext.AUTHENTICATED_SESSION_KEY);
+		if (oa != null) {
+			Boolean authenticated = (Boolean)oa;
+			st.setBoolean(8,authenticated);
+		} else {
+			st.setBoolean(8, false);
+		}
+		
+		// definido el DefaultSubjectDAO
+		Object ps = session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY);
+		if (ps != null) {
+			PrincipalCollection pc = (PrincipalCollection)ps;
+			String name = (String)pc.getPrimaryPrincipal();
+			st.setString(9,name);
+		} else {
+			st.setString(9,"nulo");
+		}
+
+		/////////////////////////////////////////////////////////////////////////
 		
 		byte[] s = serializeSession(session);
-		st.setBytes(6, s);
+		st.setBytes(10, s);
 
 	}
 	
@@ -128,8 +178,17 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 		Date created = new Date(rs.getTimestamp("created").getTime());
 		session.setStartTimestamp(created);
 		
+		Timestamp tstop = rs.getTimestamp("stoped");
+		if (tstop != null) {
+			Date stop = new Date(tstop.getTime());
+			session.setStopTimestamp(stop);
+		}
+		
 		Long timeout = rs.getLong("timeout");
 		session.setTimeout(timeout);
+		
+		boolean expired = rs.getBoolean("expired");
+		session.setExpired(expired);
 		
 		String host = rs.getString("host");
 		session.setHost(host);
@@ -144,11 +203,10 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 		try {
 			String id = UUID.randomUUID().toString();
 			((SimpleSession)session).setId(id);					// como lo hace en otras partes de shiro.
-			byte[] s = serializeSession(session);
 			
 			Connection con = getConnection();
 			try {
-				PreparedStatement st = con.prepareStatement("insert into sessions (id,created,lastAccess,timeout,host,session) values (?,?,?,?,?,?)");
+				PreparedStatement st = con.prepareStatement("insert into sessions (id,created,lastAccess,stoped,timeout,expired,host,authenticated,principal,session) values (?,?,?,?,?,?,?,?,?,?)");
 				try {
 					setSession(st, session);
 					st.executeUpdate();
@@ -221,11 +279,13 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 			
 			Connection con = getConnection();
 			try {
-				PreparedStatement st = con.prepareStatement("update sessions set id = ?, created = ?, lastAccess = ?, timeout = ?, host = ?, session = ? where id = ?");
+				PreparedStatement st = con.prepareStatement("update sessions set id = ?, created = ?, lastAccess = ?, stoped = ?, timeout = ?, expired = ?, host = ?, authenticated = ?, principal = ?, session = ? where id = ?");
 				try {
 					setSession(st, session);
-					st.setString(7,id);
-					st.executeUpdate();
+					st.setString(11,id);
+					if (st.executeUpdate() != 1) {
+						throw new UnknownSessionException();
+					}
 					
 				} finally {
 					st.close();
@@ -270,8 +330,9 @@ public class PostgreSqlDCSysSessionDAO implements SessionDAO {
 		try {
 			Connection con = getConnection();
 			try {
-				PreparedStatement st = con.prepareStatement("select * from sessions");
+				PreparedStatement st = con.prepareStatement(" select * from sessions s where s.lastAccess < ? and s.stoped is null");
 				try {
+					st.setTimestamp(1,new Timestamp((new Date()).getTime()));
 					ResultSet rs = st.executeQuery();
 					try {
 						List<Session> sessions = new ArrayList<>();
