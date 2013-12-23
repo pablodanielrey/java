559f67344506aff79f39aa5a0c335c62a1763895
pablodanielrey <pablo@econo.unlp.edu.ar>
2013-12-23 00:25:24 -0300

    agregue el evento de las personas modificadas. por lo que ahora se actualiza la vista de la persona en el caso que cambien los datos.

--- gwt/personGWT/src/main/java/ar/com/dcsys/gwt/person/client/activity/LoggedPersonActivity.java
+++ gwt/personGWT/src/main/java/ar/com/dcsys/gwt/person/client/activity/LoggedPersonActivity.java
@@ -6,10 +6,15 @@ import ar.com.dcsys.gwt.clientMessages.client.MessageDialogEvent;
 import ar.com.dcsys.gwt.manager.shared.Receiver;
 import ar.com.dcsys.gwt.person.client.gin.AssistedInjectionFactory;
 import ar.com.dcsys.gwt.person.client.manager.PersonsManager;
+import ar.com.dcsys.gwt.person.client.manager.events.PersonModifiedEvent;
+import ar.com.dcsys.gwt.person.client.manager.events.PersonModifiedEventHandler;
 import ar.com.dcsys.gwt.person.client.place.UpdatePersonDataPlace;
 
 import com.google.gwt.activity.shared.AbstractActivity;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.inject.Inject;
@@ -33,6 +38,28 @@ public class LoggedPersonActivity extends AbstractActivity {
 		this.updatePersonDataActivity = aif.updatePersonDataActivity(place);
 	}
 	
+	
+	private HandlerRegistration hr;
+	private PersonModifiedEventHandler handler = new PersonModifiedEventHandler() {
+		@Override
+		public void onPersonModified(PersonModifiedEvent event) {
+			String personId = event.getId();
+			final Person person = selection.getSelectedObject();
+			if (person == null || personId == null || person.getId() == null || (!personId.equals(person.getId()))) {
+				return;
+			}
+			
+			// si la persona que cambio es la misma que esta seleccionada, etnocnes la pido nuevamnete.
+			selection.clear();
+			Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+				@Override
+				public void execute() {
+					selectLoggedPerson();
+				}
+			});
+		}
+	};
+	
 	@Override
 	public void start(AcceptsOneWidget containerPanel, EventBus eventBus) {
 		this.eventBus = eventBus;
@@ -43,10 +70,13 @@ public class LoggedPersonActivity extends AbstractActivity {
 		updatePersonDataActivity.start(containerPanel, eventBus);
 		
 		selectLoggedPerson();
+		
+		hr = eventBus.addHandler(PersonModifiedEvent.TYPE, handler);
 	}
 	
 	@Override
 	public void onStop() {
+		hr.removeHandler();
 		selection.clear();
 		updatePersonDataActivity.onStop();
 		super.onStop();
