ff0ca1d609a88ea38d6aea939caa52c03f9806cf
emanuel <emanuel84@gmail.com>
2014-04-03 14:44:18 +0000

    Complete los metodos handler. Complete una parte del Activity GeneralsJustificationsActivity

--- gwt/assistanceGWT/src/main/java/ar/com/dcsys/gwt/assistance/client/activity/justification/GeneralsJustificationActivity.java
+++ gwt/assistanceGWT/src/main/java/ar/com/dcsys/gwt/assistance/client/activity/justification/GeneralsJustificationActivity.java
@@ -1,25 +1,108 @@
 package ar.com.dcsys.gwt.assistance.client.activity.justification;
 
+import java.util.ArrayList;
+import ar.com.dcsys.gwt.clientMessages.client.MessageDialogEvent;
 import java.util.Date;
+import java.util.List;
 
+import com.google.inject.Inject;
+
+import ar.com.dcsys.data.justification.GeneralJustificationDate;
+import ar.com.dcsys.data.justification.Justification;
+import ar.com.dcsys.gwt.assistance.client.manager.JustificationsManager;
 import ar.com.dcsys.gwt.assistance.client.ui.justification.general.GeneralsJustificationView;
+import ar.com.dcsys.gwt.assistance.shared.AssistanceFactory;
+import ar.com.dcsys.gwt.manager.shared.Receiver;
+import ar.com.dcsys.gwt.person.client.manager.events.MailChangeModifiedEvent;
 
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.view.client.MultiSelectionModel;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SelectionChangeEvent.Handler;
+import com.google.gwt.view.client.SingleSelectionModel;
 
 public class GeneralsJustificationActivity extends AbstractActivity implements GeneralsJustificationView.Presenter{
 
+	private final GeneralsJustificationView view;
+	private final AssistanceFactory assistanceFactory;
+	private final JustificationsManager justificationsManager;
+	private EventBus eventBus;
+	
+	private final SingleSelectionModel<Justification> selectionJustification;
+	private MultiSelectionModel<GeneralJustificationDate> generalJustificationSelection;
+	private final List<GeneralJustificationDate> generalJustificationDateList = new ArrayList<GeneralJustificationDate>();
+	
+	@Inject
+	public GeneralsJustificationActivity(JustificationsManager justificationsManager, AssistanceFactory assistanceFactory, GeneralsJustificationView view) {
+		this.justificationsManager = justificationsManager;
+		this.view = view;
+		this.assistanceFactory = assistanceFactory;
+		
+		selectionJustification = new SingleSelectionModel<Justification>();
+		selectionJustification.addSelectionChangeHandler(new Handler() {
+			@Override
+			public void onSelectionChange(SelectionChangeEvent event) {
+				
+			}
+		});
+		
+		generalJustificationSelection = new MultiSelectionModel<GeneralJustificationDate>();
+		generalJustificationSelection.addSelectionChangeHandler(new Handler() {
+			@Override
+			public void onSelectionChange(SelectionChangeEvent event) {
+				
+			}
+		});
+	}
+	
 	@Override
 	public void start(AcceptsOneWidget panel, EventBus eventBus) {
-		// TODO Auto-generated method stub
-		
+		 this.eventBus = eventBus;
+		 this.view.setPresenter(this);
+		 this.view.clear();
+		 
+		 view.setGeneralJustificationSelection(generalJustificationSelection);
+		 view.setTypesSelectionModel(selectionJustification);
+		 //hr = eventBus.addHandler(JustificationModifiedEvent.TYPE, handler);
+		 panel.setWidget(view);
+		 
+		 update();
+		 search(new Date(), new Date());
+	}
+	
+	private void update() {
+		justificationsManager.getJustifications(new Receiver<List<Justification>>() {
+
+			@Override
+			public void onSuccess(List<Justification> justifications) {
+				if(view == null || justifications == null || justifications.size() < 1) {
+					return;
+				}
+				view.setTypes(justifications);
+				selectionJustification.setSelected(justifications.get(0), true);
+			}
+
+			@Override
+			public void onFailure(Throwable t) {
+				showMessage(t.getMessage());
+			}
+			
+		});
 	}
 
 	@Override
 	public void persist() {
-		// TODO Auto-generated method stub
-		
+		/*if (hr != null) {
+			hr.removeHandler();
+		}*/
+		view.clear();
+		view.setPresenter(null);
+	}
+	
+	private void showMessage(String message) {
+		eventBus.fireEvent(new MessageDialogEvent(true, message));
 	}
 
 	@Override
--- gwt/assistanceModelGWT/src/main/java/ar/com/dcsys/gwt/assistance/server/FindByPersonMethodHandler.java
+++ gwt/assistanceModelGWT/src/main/java/ar/com/dcsys/gwt/assistance/server/FindByPersonMethodHandler.java
@@ -1,42 +1,70 @@
 package ar.com.dcsys.gwt.assistance.server;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import ar.com.dcsys.gwt.message.shared.MessageTransport;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import ar.com.dcsys.gwt.assistance.shared.AssistanceEncoderDecoder;
+import ar.com.dcsys.gwt.assistance.shared.AssistanceFactory;
+import ar.com.dcsys.gwt.assistance.shared.AssistanceMethods;
 import ar.com.dcsys.gwt.manager.server.AbstractMessageHandler;
 import ar.com.dcsys.gwt.message.server.MessageContext;
 import ar.com.dcsys.gwt.message.shared.Message;
 import ar.com.dcsys.gwt.message.shared.MessageUtils;
 import ar.com.dcsys.gwt.message.shared.Method;
-
-import javax.inject.Singleton;
+import ar.com.dcsys.model.justification.JustificationsManager;
 
 @Singleton
 public class FindByPersonMethodHandler extends AbstractMessageHandler {
 
+	private static final Logger logger = Logger.getLogger(FindByPersonMethodHandler.class.getName());
 	
+	private final AssistanceEncoderDecoder encoderDecoder;
+	private final MessageUtils mf;
+	private final AssistanceFactory af;
+	private final JustificationsManager justificationsManager;
+	
+	@Inject
+	public FindByPersonMethodHandler(AssistanceEncoderDecoder encoderDecoder,
+									MessageUtils messagesFactory,
+									AssistanceFactory assistanceFactory,
+									JustificationsManager justificationsManager) {
+		this.encoderDecoder = encoderDecoder;
+		this.mf = messagesFactory;
+		this.af = assistanceFactory;
+		this.justificationsManager = justificationsManager;
+	}
 	
 	@Override
 	public boolean handles(Method method) {
-		// TODO Auto-generated method stub
-		return false;
+		return AssistanceMethods.findByPerson.equals(method.getName());
 	}
 
 	@Override
 	public void handle(MessageContext ctx, Message msg, Method method) {
-		// TODO Auto-generated method stub
+		MessageTransport transport = ctx.getMessageTransport();
 		
+		try {
+			String params = method.getParams();
+			
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,e.getMessage(),e);
+			sendError(msg,transport,e.getMessage());
+		}
 	}
 
 	@Override
 	protected Logger getLogger() {
-		// TODO Auto-generated method stub
-		return null;
+		return logger;
 	}
 
 	@Override
 	protected MessageUtils getMessageUtils() {
-		// TODO Auto-generated method stub
-		return null;
+		return mf;
 	}
 
 }
