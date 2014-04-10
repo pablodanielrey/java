4fedf7cdd1767c1efba7a2b982d5b3befc18fdbf
emanuel <emanuel84@gmail.com>
2014-04-10 12:30:22 +0000

    Hice funcionar el assistance ear

--- assistance/assistanceParams/src/main/java/ar/com/dcsys/server/AssistanceParams.java
+++ assistance/assistanceParams/src/main/java/ar/com/dcsys/server/AssistanceParams.java
@@ -3,18 +3,22 @@ package ar.com.dcsys.server;
 import java.util.Date;
 import java.util.List;
 
+import ar.com.dcsys.data.device.Device;
 import ar.com.dcsys.data.justification.JustificationDAO;
+import ar.com.dcsys.data.log.AttLogDAO;
 import ar.com.dcsys.data.period.PeriodAssignation;
 import ar.com.dcsys.data.period.PeriodDAO;
 import ar.com.dcsys.data.period.PeriodType;
+import ar.com.dcsys.data.person.AssistancePersonDataDAOParams;
 import ar.com.dcsys.data.person.Person;
+import ar.com.dcsys.exceptions.DeviceException;
 import ar.com.dcsys.exceptions.PeriodException;
 import ar.com.dcsys.exceptions.PersonException;
 import ar.com.dcsys.model.PersonsManager;
 
 import javax.inject.Inject;
 
-public class AssistanceParams implements PeriodDAO.Params, JustificationDAO.Params {
+public class AssistanceParams implements PeriodDAO.Params, JustificationDAO.Params, AttLogDAO.Params, AssistancePersonDataDAOParams {
 
 	private final PersonsManager personsManager; 
 	
@@ -26,5 +30,11 @@ public class AssistanceParams implements PeriodDAO.Params, JustificationDAO.Para
 	@Override
 	public Person findPersonById(String person_id) throws PersonException {
 		return personsManager.findById(person_id);
+	}
+
+	@Override
+	public Device findDeviceById(String device_id) throws DeviceException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
