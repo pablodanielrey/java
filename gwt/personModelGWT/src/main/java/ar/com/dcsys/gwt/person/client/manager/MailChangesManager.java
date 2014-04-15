e727a8c234ff16b0a5cd849acbde42edb33b6d6b
pablodanielrey <pablo@econo.unlp.edu.ar>
2014-04-14 22:39:09 -0300

    prueba de anotacion para el annotation processor nuevo

--- gwt/personModelGWT/src/main/java/ar/com/dcsys/gwt/person/client/manager/MailChangesManager.java
+++ gwt/personModelGWT/src/main/java/ar/com/dcsys/gwt/person/client/manager/MailChangesManager.java
@@ -5,7 +5,9 @@ import java.util.List;
 import ar.com.dcsys.data.person.MailChange;
 import ar.com.dcsys.data.person.Person;
 import ar.com.dcsys.gwt.manager.shared.Receiver;
+import ar.com.dcsys.pr.ClientManager;
 
+@ClientManager
 public interface MailChangesManager {
 	
 	public void persist(MailChange mail, Person person, final Receiver<String> receiver);
