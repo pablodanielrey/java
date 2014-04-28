f9d1c735b4a02dd4915511ad519dbad27a37b934
pablodanielrey <pablo@econo.unlp.edu.ar>
2014-04-28 16:13:58 -0300

    agregue para autodescubrir transports mediante cdi.
    (new MessageTransports()).discover() --> List<Transport>

---
+++ gwt/utils/messagesGWT/src/main/java/ar/com/dcsys/gwt/messages/server/transports/MessageTransports.java
@@ -0,0 +1,8 @@
+package ar.com.dcsys.gwt.messages.server.transports;
+
+import ar.com.dcsys.gwt.messages.server.cdi.HandlersContainer;
+import ar.com.dcsys.gwt.messages.shared.Transport;
+
+public class MessageTransports extends HandlersContainer<Transport> {
+
+}
