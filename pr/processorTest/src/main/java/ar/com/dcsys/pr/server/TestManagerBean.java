796c6c33537827778dbf5190a547a459f86b7e17
pablodanielrey <pablo@econo.unlp.edu.ar>
2014-05-14 19:43:11 -0300

    genere una respuesta desde el codigo del lado del servidor para probar que la vuelta funcione ok.

--- pr/processorTest/src/main/java/ar/com/dcsys/pr/server/TestManagerBean.java
+++ pr/processorTest/src/main/java/ar/com/dcsys/pr/server/TestManagerBean.java
@@ -1,15 +1,24 @@
 package ar.com.dcsys.pr.server;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import ar.com.dcsys.gwt.manager.shared.Receiver;
 import ar.com.dcsys.gwt.messages.shared.Transport;
 import ar.com.dcsys.pr.shared.TestManager;
 
 public class TestManagerBean implements TestManager {
 
+	private static Logger logger = Logger.getLogger(TestManagerBean.class.getName());
+	
+	
 	@Override
 	public void test3(String pepe, Receiver<String> rec) {
-		// TODO Auto-generated method stub
 		
+		logger.log(Level.SEVERE, "mensaje : " + pepe);
+		logger.log(Level.SEVERE, "respondiendo : respuesta");
+		
+		rec.onSuccess("respuesta");
 	}
 
 	@Override
