2cbf2f1c788556a547df17974e65974425912367
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-06-26 21:09:39 -0300

    arregle el problema que tenia de formato de fechas y de generico de las listas.

--- pr/documentSerializers/src/main/java/ar/com/dcsys/gwt/data/document/server/DocumentListSerializer.java
+++ pr/documentSerializers/src/main/java/ar/com/dcsys/gwt/data/document/server/DocumentListSerializer.java
@@ -1,10 +1,12 @@
 package ar.com.dcsys.gwt.data.document.server;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import ar.com.dcsys.data.document.Document;
+import ar.com.dcsys.data.document.DocumentBean;
 import ar.com.dcsys.pr.CSD;
 
 import com.google.gson.Gson;
@@ -15,19 +17,23 @@ public class DocumentListSerializer implements CSD<List<Document>> {
 
 	private static final Logger logger = Logger.getLogger(DocumentListSerializer.class.getName());
 	
-	private final Gson gson = (new GsonBuilder()).create();
+	private final Gson gson = (new GsonBuilder()).setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").create();
 	
 	private class Container {
-		List<Document> list;
+		List<DocumentBean> list;
 	}	
 	
 	@Override
 	public String toJson(List<Document> o) {
 		Container sc = new Container();
-		sc.list = o;
-		String d = gson.toJson(sc);
+		List<DocumentBean> d = new ArrayList<>();
+		for (Document dc : o) {
+			d.add((DocumentBean)dc);
+		}
+		sc.list = d;
+		String ds = gson.toJson(sc);
 		logger.log(Level.WARNING, "gson : " + d);
-		return d;
+		return ds;
 	}
 
 	@Override
@@ -36,7 +42,9 @@ public class DocumentListSerializer implements CSD<List<Document>> {
 
 		TypeToken<Container> type = new TypeToken<Container>() {};
 		Container sc = gson.fromJson(json, type.getType());
-		return sc.list;
+		List<Document> l = new ArrayList<Document>();
+		l.addAll(sc.list);
+		return l;
 	}	
 	
 }
