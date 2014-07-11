5db85f138fb59d6142e7eec4ae14877dea89d74f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-07-11 13:33:01 -0300

    le hago commit a ema del proyecto del menu asi puede arrancar a programarlo.

---
+++ gwt/menu/menuGWT/src/main/java/ar/com/dcsys/gwt/menu/client/MenuGWT.java
@@ -0,0 +1,15 @@
+package ar.com.dcsys.gwt.menu.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class MenuGWT implements EntryPoint {
+
+	
+	public void onModuleLoad() {
+
+	}
+  
+}
